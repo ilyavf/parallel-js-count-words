a0099ab1e1a13e01fc729cff6e0ce5b3f700eaa2
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-01 15:20:21 -0600

    use execTask to run a task; started mapper

--- src/main.js
+++ src/main.js
@@ -1,19 +1,35 @@
 const Task = require('data.task');
 
-// mockTask :: _ -> Task
-const mockTask = (result = 'mocked task') => new Task((rej, res) => res(result))
+// toTask :: a -> Task(a)
+const toTask = (result = 'mocked task') => new Task((rej, res) => res(result))
 
-// getPage :: String -> Task
-const getPage = url => mockTask('some text here')
+// getPage :: String -> Task(String)
+const getPage = url => toTask('some text here')
 
-// countWords :: String -> Task
-const countWords = text => mockTask(text.split(' ').length)
+// countWords :: String -> Task(Number)
+const countWords = text => toTask(text.split(' ').length)
 
 let url = 'http://google.com'
 
-const app =
+// appCount :: String -> Task(Number)
+const appCount = url =>
   getPage(url)
   .chain(countWords)
 
-app.fork(e => console.log('error: ', e),
-         x => console.log('x: ', x))
+// execTask :: Task(a) -> IO()
+const execTask = t =>
+  t.fork(e => console.log('error: ', e),
+    x => console.log('x: ', x))
+
+execTask(appCount(url))
+
+const urls = [
+  'http://google.com',
+  'http://ya.ru'
+]
+
+// mapper :: Array(String) -> (String -> Task(Number)) -> Task(Array(Number))
+const mapper = (arr, func) =>
+  arr.map(func)
+
+const app = mapper(urls, appCount)

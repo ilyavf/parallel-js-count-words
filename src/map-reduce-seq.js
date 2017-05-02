936d97a207719e3950e01fc22e538c954b93153a
Ilya Fadeev <fadeev.ilya@gmail.com>
2017-05-02 13:14:36 -0600

    cleanup; normalized seq example

--- src/map-reduce-seq.js
+++ src/map-reduce-seq.js
@@ -1,8 +1,19 @@
+/**
+ * Sequential map-reduce (synchronous)
+ * @param Function mapper
+ * @param Function reducer
+ * @param Array arr Input arrayt data
+ */
+
 // map_reduce_seq :: Mapper -> Reducer -> Input
 const map_reduce_seq = (mapper, reducer, arr) =>
-  reduce_seq(reducer, arr.map(mapper))
+  reduce_seq(reducer, mapper_seq(mapper, arr))
 
-// reduce_seq :: Reducer -> Input -> Output
+// mapper_seq :: (a -> b) -> List a -> List b
+mapper_seq = (mapper, arr) =>
+  arr.map(mapper)
+
+// reduce_seq :: Reducer a b -> List a -> b
 const reduce_seq = (reducer, arr) =>
   arr.reduce(reducer)
 
